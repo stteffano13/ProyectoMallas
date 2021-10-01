ae3e57948f5a9ce6f4e32ae4fbc438cf0bd83f00
stteffano13 <tefo.aguayo@gmail.com>
2021-09-30 19:44:32 -0500

    creando menu hamburguesa

--- postCosecha/src/app/folder/folder-routing.module.ts
+++ postCosecha/src/app/folder/folder-routing.module.ts
@@ -1,17 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { componentOnReady } from '@ionic/core';
-import { BunchesComponent } from './bunches/bunches.component';
+
 
 import { FolderPage } from './folder.page';
-import { MallasComponent } from './mallas/mallas.component';
-import { MesasComponent } from './mesas/mesas.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FolderPage,
-    children:[{path:'mallas',component:MallasComponent},{path:'mesas',component:MesasComponent},{path:'bunches',component:BunchesComponent}]
+    children:[{path:'menu', loadChildren: () => import('./menu/menu.module').then( m => m.MenuModule) }]
+    
+  
   }
 ];
 
--- postCosecha/src/app/folder/menu/mallas/mallas.component.ts
+++ postCosecha/src/app/folder/menu/mallas/mallas.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class MallasComponent implements OnInit {
 
   constructor() { 
- 
+ debugger
   }
 
   ngOnInit() {}
-
+  debugger
 }
